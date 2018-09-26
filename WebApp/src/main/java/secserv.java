8f7516955e2a96008208309c264515fd6e613f7f
arunm <arunm@LAPTOP-36ED8SBJ>
2018-09-27 00:16:06 +0530

    tested redirect

--- WebApp/src/main/java/secserv.java
+++ WebApp/src/main/java/secserv.java
@@ -2,16 +2,14 @@ package main.java;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(urlPatterns="/login")
-public class FirstServlet extends HttpServlet {
+@WebServlet(urlPatterns="/login1")
+public class secserv extends HttpServlet {
 
 		/**
 	 * 
@@ -19,10 +17,14 @@ public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 		public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException{
-			System.out.println("parameter value"+req.getParameter("name"));
+			String name=req.getParameter("name");
 			PrintWriter pw= res.getWriter();
-			pw.println("XXThis is a servlet Response "+new Date());
-			pw.println("\nXXXXI got this as parameter "+req.getParameter("name"));
+			pw.println("<html>");
+			pw.println("<head>");
+			pw.println("<meta http-equiv=\"refresh\" content=\"0; url=http://www.google.com/search?q="+name+"\">");
+			pw.println("</head>");
+			pw.println("<body>Hi There</body></html>");   
+			
 			
 		}
 }
